304b24c4517f21454c65eac0047aad09fa1bbd6c
jjones <jimmy.jones@tutanota.com>
2023-10-19 20:32:41 -0600

    load data, parse and store in data store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import * as d3 from 'd3';
+	import { dataStore } from '$lib/stores/data-store';
+	import { setContext } from 'svelte';
+
+	export let data: LayoutData;
+
+	let parsedData = d3.csvParse(data.data, d3.autoType);
+
+	$: dataStore.set(parsedData);
+
+	setContext('parsedData', dataStore);
+</script>
+
 <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	// grab the chart data from the store
+	const chartData: any = getContext('parsedData');
+</script>
+
+{JSON.stringify($chartData)}
