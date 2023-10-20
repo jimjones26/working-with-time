f33307e47ec77561f1640ce410a45f599893afeb
jjones <jimmy.jones@tutanota.com>
2023-10-20 16:20:44 -0600

    add path for time and temp data

--- src/lib/components/Marks.svelte
+++ src/lib/components/Marks.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as d3 from 'd3';
 	import type { DataItem } from '$lib/stores/DataItemInterface';
 
 	export let data: Array<DataItem>;
@@ -9,6 +10,14 @@
 	export let circleRadius: number;
 </script>
 
+<path
+	fill="none"
+	stroke="#137B80"
+	d={d3
+		.line()
+		.x((d) => xScale(xValue(d)))
+		.y((d) => yScale(yValue(d)))(data)}
+/>
 {#each data as item, i}
 	<circle cx={xScale(xValue(item))} cy={yScale(yValue(item))} r={circleRadius} fill="#137B80">
 		<title>hover</title>
